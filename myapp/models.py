007be603bab69697f2aa3d2a9b926fa81d1199ea
DubaLMAO <eduthpineda04@gmail.com>
2025-12-07 17:01:28 -0600

    Evaluación inicial de escalabilidad

--- myapp/models.py
+++ myapp/models.py
@@ -10,7 +10,7 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True, null=True)
     precio = models.DecimalField(max_digits=6, decimal_places=2)
-    categoria = models.CharField(max_length=10, choices=CATEGORIAS, default='CAFE')
+    categoria = models.CharField(max_length=10, choices=CATEGORIAS, default='CAFE', db_index=True)
     imagen = models.ImageField(upload_to='productos/', blank=True, null=True)
     
     def __str__(self):
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 from .models import Producto
+from django.core.paginator import Paginator
 
 
-# Create your views here.
+
+@cache_page(60 * 15)  
 def home(request):
     cafes = Producto.objects.filter(categoria='CAFE')
     snacks = Producto.objects.filter(categoria='SNACK')
