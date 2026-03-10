976b0dcaf8b7b131ddb3032da4fd4669e287d90d
Eduth Echeverria Pineda <146026354+DubaLMAO@users.noreply.github.com>
2026-03-10 08:58:19 -0600

    Update ALLOWED_HOSTS for production environment

--- CHAED/settings.py
+++ CHAED/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = ['3.148.253.254', 'localhost']
+ALLOWED_HOSTS = ['3.149.236.54', 'localhost']
 
 CSRF_TRUSTED_ORIGINS = ['https://chaed.up.railway.app']
 
