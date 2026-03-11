fc46e07445782f65e8cc705fbc91eb1641de7ffa
DubaLMAO <eduthpineda04@gmail.com>
2026-03-11 08:28:53 -0600

    solucion para despligue en ec2

--- CHAED/settings.py
+++ CHAED/settings.py
@@ -23,14 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-dev-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = ['https://chaed.up.railway.app']
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 
 
 # Application definition
